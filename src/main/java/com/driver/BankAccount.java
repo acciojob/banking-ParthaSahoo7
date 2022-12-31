76b9428aa4d09bfd58dcf2541ae70c168e65f0a1
Partha sarathi <parthasahoo2677@gmail.com>
2022-12-31 11:28:27 +0530

    first

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -5,8 +5,49 @@ public class BankAccount {
     private String name;
     private double balance;
     private double minBalance;
+    private String accountNumber;
+
+    public BankAccount() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getMinBalance() {
+        return minBalance;
+    }
+
+    public void setMinBalance(double minBalance) {
+        this.minBalance = minBalance;
+    }
 
     public BankAccount(String name, double balance, double minBalance) {
+        this.balance=balance;
+        this.name=name;
+        this.minBalance=minBalance;
+
+    }
+
+    public void createAccount(String str, int digit,int sum){
+        if(str.length()>=digit){
+            int count=0;   //checking its length
+            for(int i=0;i<str.length();i++) count+=(str.charAt(i)-'a');
+            if(count==sum) this.accountNumber=str;
+            return;
+        }
 
     }
 
@@ -15,16 +56,31 @@ public class BankAccount {
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
 
-        return null;
+        createAccount("",digits,sum);
+        return accountNumber;
+
+
     }
 
     public void deposit(double amount) {
         //add amount to balance
+        this.balance+=amount;
 
+    }
+    class InsufficientBalanceException extends RuntimeException{
+        public InsufficientBalanceException(String errorMessage){
+            super(errorMessage);}
     }
 
     public void withdraw(double amount) throws Exception {
         // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
+
+        if(this.balance-amount<minBalance) throw new InsufficientBalanceException("Insufficient Balance");
+        else{
+            this.balance=this.balance-amount;
+        }
+
+
 
     }
 
--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -4,8 +4,21 @@ public class SavingsAccount extends BankAccount{
     double rate;
     double maxWithdrawalLimit;
 
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    public double getMaxWithdrawalLimit() {
+        return maxWithdrawalLimit;
+    }
+
+    public void setMaxWithdrawalLimit(double maxWithdrawalLimit) {
+        this.maxWithdrawalLimit = maxWithdrawalLimit;
+    }
+
     public SavingsAccount(String name, double balance, double maxWithdrawalLimit, double rate) {
         // minimum balance is 0 by default
+        super(name,balance,0.0);
 
     }
     public void withdraw(double amount) throws Exception {
@@ -13,16 +26,23 @@ public class SavingsAccount extends BankAccount{
         // 1. "Maximum Withdraw Limit Exceed" : If the amount exceeds maximum withdrawal limit
         // 2. "Insufficient Balance" : If the amount exceeds balance
 
+        if(amount>maxWithdrawalLimit) throw new InsufficientBalanceException("Maximum Withdraw Limit Exceed");
+        if(amount>this.getBalance()) throw new InsufficientBalanceException("Insufficient Balance");
+
     }
 
     public double getSimpleInterest(int years){
         // Return the final amount considering that bank gives simple interest on current amount
 
+        double amount=(years*this.getBalance()*1)/100;
+        return amount;
     }
 
     public double getCompoundInterest(int times, int years){
         // Return the final amount considering that bank gives compound interest on current amount given times per year
 
+        double ci=this.getBalance()*Math.pow(1+(rate / 100),years);
+        return ci;
     }
 
 }
